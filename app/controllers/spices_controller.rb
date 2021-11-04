6dabd8138d3748e6b7e93d14c6fc40022025f73a
jessicasewell <sewell.jessica95@gmail.com>
2021-11-04 19:22:53 -0400

    Done

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,32 @@
+class SpicesController < ApplicationController
+    def index
+        spices = Spice.all
+        render json: spices
+      end
+    
+    #   def show
+    #     spice = Spice.find_by(id: params[:id])
+    #     render json: spice
+    #   end
+    
+      def update
+       spice = Spice.find_by(id: params[:id])
+        spice.update(spice_params)
+        render json: spice
+      end
+    
+      def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+      end
+    
+      def destroy
+        spice = Spice.find_by(id: params[:id])
+        spice.destroy
+        render json: spice
+      end
+    
+      def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+      end
+end 
